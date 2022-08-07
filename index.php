47f715b22d057d584fbdd5f947ded099fad159a4
balayokesh <balayokeshmani@gmail.com>
2022-08-07 15:08:32 +0530

    removed design from template

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -137,14 +136,5 @@
 					</ul>
 				</div>
 			</section>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-
 	</body>
 </html>
