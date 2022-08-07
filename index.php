df75adb6fa39a2769867f0a0ec97c49d5576725c
balayokesh <balayokeshmani@gmail.com>
2022-08-07 16:00:41 +0530

    disabled google login temporarily

--- index.php
+++ index.php
@@ -16,51 +16,51 @@
 			<p>Media where creative minds meet together</p>
 
 			<?php 
-				session_start();
+				// session_start();
 
-				//If already signed in then redirect to app.php
+				// //If already signed in then redirect to app.php
 
-				if(isset($_SESSION['user_mailid'])){
-					header("location: app.php");
-				}  
+				// if(isset($_SESSION['user_mailid'])){
+				// 	header("location: app.php");
+				// }  
 
-				require_once 'client/vendor/autoload.php';
+				// require_once 'client/vendor/autoload.php';
 
-				// init configurations
-				$clientId = "732279262537-gnfe3jf4ba77unsbpupsp893v425m2rk.apps.googleusercontent.com";
-				$clientSecret = "bfqHUV0Y1jlrB8kpyUHVpFqF";
-				$redirectUri = "http://localhost/creative-media/index.php";
+				// // init configurations
+				// $clientId = "732279262537-gnfe3jf4ba77unsbpupsp893v425m2rk.apps.googleusercontent.com";
+				// $clientSecret = "bfqHUV0Y1jlrB8kpyUHVpFqF";
+				// $redirectUri = "http://localhost/creative-media/index.php";
 
-				// Creating client request
-				$client = new Google_Client();
-				$client->setClientId($clientId);
-				$client->setClientSecret($clientSecret);
-				$client->setRedirectUri($redirectUri);
+				// // Creating client request
+				// $client = new Google_Client();
+				// $client->setClientId($clientId);
+				// $client->setClientSecret($clientSecret);
+				// $client->setRedirectUri($redirectUri);
 
-				$client->addScope('email');
-				$client->addScope('profile');
+				// $client->addScope('email');
+				// $client->addScope('profile');
 
-				if(isset($_GET['code'])){
-					$token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-					$client->setAccessToken($token['access_token']);
+				// if(isset($_GET['code'])){
+				// 	$token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+				// 	$client->setAccessToken($token['access_token']);
 
-					// Getting profile info
-					$google_oauth = new Google_Service_Oauth2($client);
-					$google_account_info = $google_oauth->userinfo->get();
+				// 	// Getting profile info
+				// 	$google_oauth = new Google_Service_Oauth2($client);
+				// 	$google_account_info = $google_oauth->userinfo->get();
 
-					$email = $google_account_info->email;
-					$name = $google_account_info->name;
-					$picture = $google_account_info->picture;
+				// 	$email = $google_account_info->email;
+				// 	$name = $google_account_info->name;
+				// 	$picture = $google_account_info->picture;
 
-					$_SESSION['user_mailid'] = $email;
-					$_SESSION['user_name'] = $name;
-					$_SESSION['profile_picture'] = $picture;
-					header("location: app.php");
-				}
-				else{
-					echo "<a class='button' id='google-login' href=". $client->createAuthUrl().">Log in with Google</a>";
-					echo "<a href='app.php'></a>";
-				}
+				// 	$_SESSION['user_mailid'] = $email;
+				// 	$_SESSION['user_name'] = $name;
+				// 	$_SESSION['profile_picture'] = $picture;
+				// 	header("location: app.php");
+				// }
+				// else{
+				// 	echo "<a class='button' id='google-login' href=". $client->createAuthUrl().">Log in with Google</a>";
+				// 	echo "<a href='app.php'></a>";
+				// }
 			?>
 
 		</header>
